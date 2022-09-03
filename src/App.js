61b622f730da6b1ac2cf594a73b0eab3f541e676
DKatuk <dilarakatuk@gmail.com>
2022-09-03 19:58:18 +0300

    created react app - configured tailwindcss - enabled absolute imports

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-primary-200 text-black">Welcome to my Todo App</h1>
+    </div>
+  );
+}
+
+export default App;
